0cea53ecc4f18ad936d4d5115aed34e6f962f529
Austin Ho <austinho@Austins-MacBook-Pro.local>
2018-10-31 13:07:25 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# GunnHacks 4.0
+
+Make, Build, Create & Learn. It’s GunnHacks4.0, Gunn’s 24-hour high school hackathon.
+
+Sign up for updates at www.gunnhacks.com.
---
+++ js/main.js
@@ -0,0 +1,199 @@
+window.onload = function() {
+    var questions = $(".question");
+    questions[0].onclick = function() {
+        if($(questions[0])[0].nextElementSibling.style.display == "none"){
+            $(questions[0])[0].nextElementSibling.style.display = "block";
+            $(questions[0]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[0])[0].nextElementSibling.style.display == "block"){
+            $(questions[0])[0].nextElementSibling.style.display = "none";    
+            $(questions[0]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[0])[0].nextElementSibling.style.display = "block";
+            $(questions[0]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[1].onclick = function() {
+        if($(questions[1])[0].nextElementSibling.style.display == "none"){
+            $(questions[1])[0].nextElementSibling.style.display = "block";
+            $(questions[1]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[1])[0].nextElementSibling.style.display == "block"){
+            $(questions[1])[0].nextElementSibling.style.display = "none";    
+            $(questions[1]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[1])[0].nextElementSibling.style.display = "block";
+            $(questions[1]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[2].onclick = function() {
+        if($(questions[2])[0].nextElementSibling.style.display == "none"){
+            $(questions[2])[0].nextElementSibling.style.display = "block";
+            $(questions[2]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[2])[0].nextElementSibling.style.display == "block"){
+            $(questions[2])[0].nextElementSibling.style.display = "none";    
+            $(questions[2]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[2])[0].nextElementSibling.style.display = "block";
+            $(questions[2]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[3].onclick = function() {
+        if($(questions[3])[0].nextElementSibling.style.display == "none"){
+            $(questions[3])[0].nextElementSibling.style.display = "block";
+            $(questions[3]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[3])[0].nextElementSibling.style.display == "block"){
+            $(questions[3])[0].nextElementSibling.style.display = "none";    
+            $(questions[3]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[3])[0].nextElementSibling.style.display = "block";
+            $(questions[3]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[4].onclick = function() {
+        if($(questions[4])[0].nextElementSibling.style.display == "none"){
+            $(questions[4])[0].nextElementSibling.style.display = "block";
+            $(questions[4]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[4])[0].nextElementSibling.style.display == "block"){
+            $(questions[4])[0].nextElementSibling.style.display = "none";    
+            $(questions[4]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[4])[0].nextElementSibling.style.display = "block";
+            $(questions[4]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[5].onclick = function() {
+        if($(questions[5])[0].nextElementSibling.style.display == "none"){
+            $(questions[5])[0].nextElementSibling.style.display = "block";
+            $(questions[5]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[5])[0].nextElementSibling.style.display == "block"){
+            $(questions[5])[0].nextElementSibling.style.display = "none";    
+            $(questions[5]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[5])[0].nextElementSibling.style.display = "block";
+            $(questions[5]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[6].onclick = function() {
+        if($(questions[6])[0].nextElementSibling.style.display == "none"){
+            $(questions[6])[0].nextElementSibling.style.display = "block";
+            $(questions[6]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[6])[0].nextElementSibling.style.display == "block"){
+            $(questions[6])[0].nextElementSibling.style.display = "none";    
+            $(questions[6]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[6])[0].nextElementSibling.style.display = "block";
+            $(questions[6]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[7].onclick = function() {
+        if($(questions[7])[0].nextElementSibling.style.display == "none"){
+            $(questions[7])[0].nextElementSibling.style.display = "block";
+            $(questions[7]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[7])[0].nextElementSibling.style.display == "block"){
+            $(questions[7])[0].nextElementSibling.style.display = "none";    
+            $(questions[7]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[7])[0].nextElementSibling.style.display = "block";
+            $(questions[7]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[8].onclick = function() {
+        if($(questions[8])[0].nextElementSibling.style.display == "none"){
+            $(questions[8])[0].nextElementSibling.style.display = "block";
+            $(questions[8]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[8])[0].nextElementSibling.style.display == "block"){
+            $(questions[8])[0].nextElementSibling.style.display = "none";    
+            $(questions[8]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[8])[0].nextElementSibling.style.display = "block";
+            $(questions[8]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[9].onclick = function() {
+        if($(questions[9])[0].nextElementSibling.style.display == "none"){
+            $(questions[9])[0].nextElementSibling.style.display = "block";
+            $(questions[9]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[9])[0].nextElementSibling.style.display == "block"){
+            $(questions[9])[0].nextElementSibling.style.display = "none";    
+            $(questions[9]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[9])[0].nextElementSibling.style.display = "block";
+            $(questions[9]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[10].onclick = function() {
+        if($(questions[10])[0].nextElementSibling.style.display == "none"){
+            $(questions[10])[0].nextElementSibling.style.display = "block";
+            $(questions[10]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[10])[0].nextElementSibling.style.display == "block"){
+            $(questions[10])[0].nextElementSibling.style.display = "none";    
+            $(questions[10]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[10])[0].nextElementSibling.style.display = "block";
+            $(questions[10]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[11].onclick = function() {
+        if($(questions[11])[0].nextElementSibling.style.display == "none"){
+            $(questions[11])[0].nextElementSibling.style.display = "block";
+            $(questions[11]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[11])[0].nextElementSibling.style.display == "block"){
+            $(questions[11])[0].nextElementSibling.style.display = "none";    
+            $(questions[11]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[11])[0].nextElementSibling.style.display = "block";
+            $(questions[11]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[12].onclick = function() {
+        if($(questions[12])[0].nextElementSibling.style.display == "none"){
+            $(questions[12])[0].nextElementSibling.style.display = "block";
+            $(questions[12]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[12])[0].nextElementSibling.style.display == "block"){
+            $(questions[12])[0].nextElementSibling.style.display = "none";    
+            $(questions[12]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[12])[0].nextElementSibling.style.display = "block";
+            $(questions[12]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+    questions[13].onclick = function() {
+        if($(questions[13])[0].nextElementSibling.style.display == "none"){
+            $(questions[13])[0].nextElementSibling.style.display = "block";
+            $(questions[13]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";
+        }
+        else if($(questions[13])[0].nextElementSibling.style.display == "block"){
+            $(questions[13])[0].nextElementSibling.style.display = "none";    
+            $(questions[13]).find(".symbol").find("h3")[0].innerHTML = "+";            
+        }
+        else{
+            $(questions[13])[0].nextElementSibling.style.display = "block";
+            $(questions[13]).find(".symbol").find("h3")[0].innerHTML = "&ndash;";            
+        }
+    }
+}
